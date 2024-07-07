7ac049a25e5dfbde23fe90e98b3f7a0a414433f6
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2024-07-07 13:45:28 -0300

    feat: refactored SignUp service to improve data validation

--- src/application/services/signup.ts
+++ src/application/services/signup.ts
@@ -1,8 +1,8 @@
 import crypto from "crypto";
+import validateName from "src/utils/validate-name";
 import validateCarPlate from "src/utils/validateCarPlate";
 import { validateCpf } from "src/utils/validateCpf";
 import validateEmail from "src/utils/validateEmail";
-import validateName from "src/utils/validateName";
 import type { IUserRepository } from "../protocols/user-repository";
 
 type Input = {
@@ -14,22 +14,23 @@ type Input = {
   isDriver: boolean;
 };
 
+type Output = {
+  accountId: string;
+};
+
 export default class SignUp {
   constructor(private readonly userRepository: IUserRepository) {}
 
-  async execute(input: Input): Promise<any> {
+  async execute(input: Input): Promise<Output> {
     try {
       const id = crypto.randomUUID();
 
       const acc = await this.userRepository.get({ email: input.email });
 
-      if (acc) return "Email already in use.";
+      if (acc) throw new Error("Email already in use.");
 
-      if (!validateName(input.name)) return "Invalid name.";
-      if (!validateEmail(input.email)) return "Invalid email.";
-      if (!validateCpf(input.cpf)) return "Invalid CPF.";
-      if (!validateCarPlate(input.carPlate) && input.isDriver)
-        return "Invalid car plate.";
+      const validation = this.validateData({ ...input });
+      if (validation) throw new Error(validation);
 
       await this.userRepository.save({ ...input, accountId: id });
 
@@ -37,9 +38,19 @@ export default class SignUp {
         accountId: id,
       };
     } catch (error: any) {
-      console.log(error);
-
-      return -6;
+      throw new Error(error);
     }
+  }
+
+  private validateData(input: {
+    name: string;
+    email: string;
+    cpf: string;
+    carPlate: string;
+  }) {
+    if (!validateName(input.name)) return "Invalid name.";
+    if (!validateEmail(input.email)) return "Invalid email.";
+    if (!validateCpf(input.cpf)) return "Invalid CPF.";
+    if (!validateCarPlate(input.carPlate)) return "Invalid car plate.";
   }
 }
