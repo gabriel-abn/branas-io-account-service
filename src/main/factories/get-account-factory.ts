88e43014e95f429f701e519cb04a847768436e99
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2024-07-08 00:21:25 -0300

    feat: implemented factories

--- src/main/factories/get-account-factory.ts
+++ src/main/factories/get-account-factory.ts
@@ -1,9 +1,14 @@
 import GetAccount from "src/application/services/get-account";
 import UserRepository from "src/infra/repositories/user-repository";
+import GetAccountController from "src/presentation/controllers/get-account-controller";
 
 export default class GetAccountFactory {
   static makeUseCase(): GetAccount {
     const userRepository = new UserRepository();
     return new GetAccount(userRepository);
+  }
+
+  static makeController(): GetAccountController {
+    return new GetAccountController(this.makeUseCase());
   }
 }
--- src/main/factories/sign-up-factory.ts
+++ src/main/factories/sign-up-factory.ts
@@ -1,9 +1,14 @@
 import SignUp from "src/application/services/signup";
 import UserRepository from "src/infra/repositories/user-repository";
+import SignUpController from "src/presentation/controllers/sign-up-controller";
 
 export default class SignUpFactory {
   static makeUseCase(): SignUp {
     const userRepository = new UserRepository();
     return new SignUp(userRepository);
+  }
+
+  static makeController(): SignUpController {
+    return new SignUpController(this.makeUseCase());
   }
 }
